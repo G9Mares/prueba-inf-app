03ec792c3516393228a66ccbeb40ed4230e6cbfe
GMares <gustavo99.mares@gmail.com>
2023-07-20 15:43:55 -0600

    cambio del icono

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,5 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Iceberg, Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,6 +14,7 @@ export default function RootLayout({ children }) {
       <head>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css"/>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous"/>
+        <link rel='icon' href='/logo_inein.png' type='image/png'/>
       </head>
       <body className={inter.className}>
         {children}
